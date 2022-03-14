f7e9e2d3f2ca2c20ca5242b698bb0caa2660b57d
Owner <97609133+go-compile@users.noreply.github.com>
2022-03-14 12:23:29 +0000

    FIX: run relay: grab config file names from args

--- cmd/localrelay/run.go
+++ cmd/localrelay/run.go
@@ -5,17 +5,18 @@ import (
 	"os"
 
 	"github.com/naoina/toml"
+	"github.com/pkg/errors"
 )
 
 func runRelays(opt *options, i int, cmd []string) error {
 
 	// Read all relay config files and decode them
-	relays := make([]Relay, 0, len(cmd[1:]))
-	for _, file := range cmd[i:] {
+	relays := make([]Relay, 0, len(cmd[i+1:]))
+	for _, file := range cmd[i+1:] {
 
 		f, err := os.Open(file)
 		if err != nil {
-			return err
+			return errors.Wrapf(err, "file:%q", file)
 		}
 
 		var relay Relay
@@ -27,6 +28,11 @@ func runRelays(opt *options, i int, cmd []string) error {
 		relays = append(relays, relay)
 
 		f.Close()
+	}
+
+	if len(relays) == 0 {
+		fmt.Println("[WARN] No relay configs provided.")
+		return nil
 	}
 
 	fmt.Println(relays)
