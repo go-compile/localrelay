2d45dd3255239697e1e1524a65eefd7b1455440a
Go Compile <97609133+go-compile@users.noreply.github.com>
2022-06-03 19:09:00 +0100

    CHANGE: metrics view layout

--- cmd/localrelay/metrics.go
+++ cmd/localrelay/metrics.go
@@ -54,9 +54,9 @@ func relayMetrics(opt *options) error {
 		case <-sig:
 			// make a guess how far to move the cursor
 			if len(relays) != 0 {
-				fmt.Printf("\x1b[%dB", len(relays)+2)
+				fmt.Printf("\x1b[%dB", (len(relays)*2)+2)
 			} else {
-				fmt.Printf("\x1b[%dB", len(status.Metrics)+2)
+				fmt.Printf("\x1b[%dB", (len(status.Metrics)*2)+2)
 			}
 
 			return nil
@@ -71,9 +71,9 @@ func relayMetrics(opt *options) error {
 				metrics = append(metrics, namedMetrics{k, m})
 			}
 
-			// sort by bandwidth
+			// sort alphabetically
 			sort.SliceStable(metrics, func(i, j int) bool {
-				return metrics[i].name > metrics[j].name
+				return metrics[i].name < metrics[j].name
 			})
 
 			count := len(relays)
@@ -98,12 +98,12 @@ func relayMetrics(opt *options) error {
 			}
 
 			fmt.Printf("\r\n\x1b[2K [Running Relays: %d]\r\n", len(status.Metrics))
-			fmt.Printf("\x1b[%dA", count+2)
+			fmt.Printf("\x1b[%dA", (count*2)+2)
 			time.Sleep(opt.interval)
 		}
 	}
 }
 
 func printMetrics(name string, m metrics) {
-	fmt.Printf("\x1b[2K%s: [In/Out:%s/%s] [DialAvg:%dms] [Active:%d] [Total:%d]\r\n", name, formatBytes(m.In), formatBytes(m.Out), m.DialAvg, m.Active, m.TotalConns+m.TotalRequests)
+	fmt.Printf("\x1b[2K \x1b[90m%s\x1b[0m\r\n\x1b[2K  [In/Out:%s/%s] [DialAvg:%dms] [Active:%d] [Total:%d]\r\n", name, formatBytes(m.In), formatBytes(m.Out), m.DialAvg, m.Active, m.TotalConns+m.TotalRequests)
 }
