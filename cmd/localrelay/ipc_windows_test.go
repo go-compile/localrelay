dd9ff5b2d8b30ebf3808bc53e6e40b71f57250b3
Go Compile <97609133+go-compile@users.noreply.github.com>
2023-01-06 14:11:38 +0000

    fix(testing): t.Fatal on non-test gorutine resulting in panic

--- cmd/localrelay/ipc_windows_test.go
+++ cmd/localrelay/ipc_windows_test.go
@@ -8,11 +8,20 @@ import (
 func TestIPCWindows(t *testing.T) {
 	go func() {
 		if err := IPCListen(); err != nil {
+			// if IPC listen fails make sure your host system isn't
+			// already running localrelay. Run localrelay stop
 			t.Fatal(err)
 		}
 	}()
 
 	time.Sleep(time.Millisecond * 50)
+
+	// due to the above embeded function being in a different
+	// gorutine, t.Fatal will only effect the above subroutine.
+	// Hence needing to check if ipcListener is nil.
+	if ipcListener == nil {
+		t.Fatal("ipc listener could not startup")
+	}
 
 	_, err := serviceStatus()
 	if err != nil {
