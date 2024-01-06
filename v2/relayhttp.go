62a9350340d265d85ad57756e818e22eac08294b
go-compile <97609133+go-compile@users.noreply.github.com>
2024-01-06 18:31:31 +0000

    feat: http relay proxy

--- v2/relayhttp.go
+++ v2/relayhttp.go
@@ -27,8 +27,10 @@ func HandleHTTP(relay *Relay) http.HandlerFunc {
 func handleHTTP(w http.ResponseWriter, r *http.Request, re *Relay) {
 	re.Metrics.requests(1)
 
+	destination := re.Destination[0]
+
 	// FIXME: force only one destination for http(s) relays
-	remoteURL := re.Destination[0].Addr() + r.URL.Path + "?" + r.URL.Query().Encode()
+	remoteURL := destination.Protocol() + "://" + destination.Addr() + r.URL.Path + "?" + r.URL.Query().Encode()
 
 	// BUG: sometimes requests redirect and cause a loop (Loop is auto stopped)
 	req, err := http.NewRequest(r.Method, remoteURL, r.Body)
@@ -44,12 +46,41 @@ func handleHTTP(w http.ResponseWriter, r *http.Request, re *Relay) {
 		req.Header.Set(k, strings.Join(v, ","))
 	}
 
+	// used to record dial time
 	start := time.Now()
-	response, err := re.httpClient.Do(req)
+
+	// clone http client, as to not cause a race condition when we apply a proxy
+	hclient := cloneHttpClient(*re.httpClient)
+
+	proxyStrings, proxyNames, err := destination.Proxy(re)
+	if err != nil {
+		re.logger.Error.Printf("destination proxy error: %s\n", err)
+		return
+	}
+
+	if len(proxyNames) == 0 {
+		forwardHttp(&hclient, re, req, w, start)
+		return
+	}
+
+	for _, proxyString := range proxyStrings {
+		hclient.Transport = &http.Transport{
+			Proxy: http.ProxyURL(proxyString.URL),
+		}
+
+		if forwardHttp(&hclient, re, req, w, start) {
+			// success
+			return
+		}
+	}
+}
+
+func forwardHttp(hclient *http.Client, re *Relay, req *http.Request, w http.ResponseWriter, start time.Time) bool {
+	response, err := hclient.Do(req)
 	if err != nil {
 		re.logger.Error.Println("FORWARD REQUEST ERROR: ", err)
 		re.Metrics.dial(0, 1, start)
-		return
+		return false
 	}
 
 	re.Metrics.dial(1, 0, start)
@@ -66,4 +97,10 @@ func handleHTTP(w http.ResponseWriter, r *http.Request, re *Relay) {
 	in, _ := io.Copy(w, response.Body)
 	re.Metrics.bandwidth(0, int(in))
 
+	return true
+}
+
+func cloneHttpClient(client http.Client) http.Client {
+	c := client
+	return c
 }
