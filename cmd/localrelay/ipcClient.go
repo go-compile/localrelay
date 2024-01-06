14743ba141e35fd4010686bf761be2b34cb51a66
go-compile <97609133+go-compile@users.noreply.github.com>
2024-01-06 15:13:39 +0000

    refactor: ipc client to use lib

--- cmd/localrelay/ipcClient.go
+++ cmd/localrelay/ipcClient.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"net/url"
-
-	"github.com/go-compile/localrelay/internal/ipc"
 	"github.com/go-compile/localrelay/pkg/api"
-	"github.com/pkg/errors"
 )
 
 // serviceRun takes paths to relay config files and then connects via IPC to
@@ -47,121 +42,73 @@ func serviceStatus() (*api.Status, error) {
 }
 
 func stopRelay(relayName string) error {
-	client, conn, err := ipc.Connect()
+	c, err := api.Connect()
 	if err != nil {
 		return err
 	}
 
-	defer conn.Close()
+	defer c.Close()
 
-	resp, err := client.Get("http://lr/stop/" + url.PathEscape(relayName))
-	if err != nil {
-		return err
-	}
-
-	switch resp.StatusCode {
-	case 200:
+	if err := c.StopRelay(relayName); err == nil {
 		Printf("Relay %q has been stopped.\n", relayName)
-	case 500:
-		Printf("Failed to stop relay.\n")
-	case 404:
-		Printf("Relay not found.\n")
-	default:
-		Printf("Unknown response %d.\n", resp.StatusCode)
+		return nil
 	}
 
-	return nil
+	return err
 }
 
 func activeConnections() ([]api.Connection, error) {
-	client, conn, err := IPCConnect()
+	c, err := api.Connect()
 	if err != nil {
 		return nil, err
 	}
 
-	defer conn.Close()
+	defer c.Close()
 
-	resp, err := client.Get("http://lr/connections")
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != 200 {
-		return nil, errors.New("failed to fetch connections")
-	}
-
-	var pool []connection
-	if err := json.NewDecoder(resp.Body).Decode(&pool); err != nil {
-		return nil, err
-	}
-
-	return pool, nil
+	return c.GetConnections()
 }
 
 func dropAll() error {
-	client, conn, err := IPCConnect()
+	c, err := api.Connect()
 	if err != nil {
 		return err
 	}
 
-	defer conn.Close()
+	defer c.Close()
 
-	resp, err := client.Get("http://lr/drop")
-	if err != nil {
-		return err
-	}
-
-	switch resp.StatusCode {
-	case 200:
-		Printf("All connections have been dropped.\r\n")
-	default:
-		Printf("Failed to drop connections. Status code: %d.\r\n", resp.StatusCode)
-	}
-
-	return nil
+	return c.DropAll()
 }
 
 func dropIP(ip string) error {
-	client, conn, err := IPCConnect()
+	c, err := api.Connect()
 	if err != nil {
 		return err
 	}
 
-	defer conn.Close()
+	defer c.Close()
 
-	resp, err := client.Get("http://lr/drop/ip/" + url.PathEscape(ip))
-	if err != nil {
-		return err
-	}
-
-	switch resp.StatusCode {
-	case 200:
+	if err := c.DropIP(ip); err == nil {
 		Printf("All connections from %q have been dropped.\r\n", ip)
-	default:
-		Printf("Failed to drop connections. Status code: %d.\n", resp.StatusCode)
+		return nil
 	}
 
+	Printf("Failed to drop connections. Err: %s.\n", err)
 	return nil
 }
 
 func dropRelay(relay string) error {
-	client, conn, err := IPCConnect()
+	c, err := api.Connect()
 	if err != nil {
 		return err
 	}
 
-	defer conn.Close()
+	defer c.Close()
 
-	resp, err := client.Get("http://lr/drop/relay/" + url.PathEscape(relay))
-	if err != nil {
-		return err
-	}
-
-	switch resp.StatusCode {
-	case 200:
+	if err := c.DropRelay(relay); err == nil {
 		Printf("All connections from %q have been dropped.\r\n", relay)
-	default:
-		Printf("Failed to drop connections. Status code: %d.\n", resp.StatusCode)
+		return nil
+	} else {
+		Printf("Failed to drop connections. Status code: %s.\n", err)
 	}
 
 	return nil
