3bd2bd265dacffa97eeb4c3e1de4041af9aaa51a
go-compile <97609133+go-compile@users.noreply.github.com>
2024-01-07 21:16:34 +0000

    feat: update basic example to v2

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/go-compile/localrelay"
+	"github.com/go-compile/localrelay/v2"
 )
 
 func main() {
@@ -11,7 +11,10 @@ func main() {
 	// nextcloud is the name of the relay. Note this can be called anything
 	// 127.0.0.1:90 is the address the relay will listen on. E.g. you connect via localhost:90
 	// localhost:8080 is the destination address, this can be a remote server
-	r := localrelay.New("nextcloud", "127.0.0.1:90", "localhost:8080", os.Stdout)
+	r, err := localrelay.New("nextcloud", os.Stdout, "tcp://127.0.0.1:90", "tcp://localhost:8080")
+	if err != nil {
+		panic(err)
+	}
 
 	// Starts the relay server
 	panic(r.ListenServe())
