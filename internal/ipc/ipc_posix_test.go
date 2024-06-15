2b4aed1d6bdffe7cd211fd21a1445d7d69f55ded
go-compile <97609133+go-compile@users.noreply.github.com>
2024-06-15 10:16:05 +0100

    fix: ipc listen test package name

--- internal/ipc/ipc_posix_test.go
+++ internal/ipc/ipc_posix_test.go
@@ -1,7 +1,7 @@
 //go:build darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris || !windows
 // +build darwin dragonfly freebsd linux netbsd openbsd solaris !windows
 
-package ipc_test
+package ipc
 
 func init() {
 	ipcPathPrefix = "./"
